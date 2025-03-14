0b62438b1bec9176dac7a5ff6e7e5d4c038603e8
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-03-14 13:50:10 -0400

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # sends a GET request to the URL passed in on initialization
+        # should return the body of the response
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        # should use get_response_body to send a request
+        # then returns a Python list or dictionary made up of data converted from the response of that request
+        response_body = self.get_response_body()
+        return json.loads(response_body)
